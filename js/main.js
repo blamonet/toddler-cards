76946bd5be14e8c2b4b8362f62a21d6cbb213b19
blamonet <blamonet@gmail.com>
2013-11-25 19:08:25 -0800

    Closed #5 Added category filter

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-var dictionary = {};
+var dictionary = [];
+var dictionary_by_category = [];
+var category = 'all';
+
 var currentWord = 0;
 
 function playSound() {
@@ -10,11 +13,30 @@ function nextWord() {
   var newWord = currentWord;
   var sound = "";
   var sound_audio_tag = "";
-  while (newWord == currentWord) {
-    newWord=Math.floor(Math.random()*dictionary.length);
+  var dictionary_length = "";
+  if (category == "all") {
+    dictionary_length = dictionary.length;
+  } else {
+    dictionary_length = dictionary_by_category.length;
+  }
+  if (dictionary_length > 1) {
+    while (newWord == currentWord) {
+      if (category == "all") {
+        newWord=Math.floor(Math.random()*dictionary.length);
+      } else {
+        newWord=Math.floor(Math.random()*dictionary_by_category.length);
+      }
+    }
+  } else {
+    newWord = 0;    
   }
   currentWord = newWord;
-  var entry = dictionary[currentWord];
+  var entry;
+  if (category == "all") {
+    entry = dictionary[currentWord];
+  } else {
+    entry = dictionary_by_category[currentWord];
+  }
   var image = entry.img[Math.floor(Math.random()*entry.img.length)];
   if (entry.sound && entry.sound.length > 0) {
     var sound_audio_tag = "<audio id=\"mysoundclip\" preload=\"auto\"><source src=\"sounds/" + entry.sound[0] + "\"></source></audio>"
@@ -22,6 +44,23 @@ function nextWord() {
   }
   $(".word").html(entry.title + " " + sound + sound_audio_tag);
   $(".wordImage img").attr("src","img/" + image);
+}
+
+function populateCategoryDictionary() {
+    dictionary_by_category = [];
+    if (category == "all") {
+      dictionary_by_category = dictionary;
+    } else {
+      $.each(dictionary, function(i, item) {
+        $.each(item.category, function(j, _category) {
+          if (_category == category) {
+            dictionary_by_category.push(item);
+          }
+        });
+      });
+    }
+    $(".categorySelector").blur();
+    nextWord();
 }
 
 function addCategory(categoryName) {
@@ -44,13 +83,17 @@ $(document).ready(function() {
   $.getJSON("items.json", function (data) {
     dictionary = data.dictionary;
     $.each(dictionary, function(i, item) {
-      $.each(item.category, function(j, category) {
-        if (!categoryInList(category)) {
-          addCategory(category);
+      $.each(item.category, function(j, _category) {
+        if (!categoryInList(_category)) {
+          addCategory(_category);
         }
       });
     });
     nextWord();
+  });
+  $(".categorySelector").change(function() {
+     category = $(".categorySelector option:selected").text();
+     populateCategoryDictionary();
   });
 });
 
